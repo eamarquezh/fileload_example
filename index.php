86b8bb78e9b08425fdd021cd627502b10f83ae9c
eamarquezh <supericman@outlook.com>
2023-06-19 20:44:15 -0600

    just file load

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>load file</title>
+</head>
+<body>
+<input type="file" name="miArchivo" id="miArchivo"> 
+<br>
+<button id="botonCarga">subir archivo</button>
+
+<script src="main.js"></script>
+
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,30 @@
+
+const miArchivo = document.getElementById('miArchivo');
+const botonCarga = document.getElementById('botonCarga');
+
+botonCarga.addEventListener('click',()=>{uploadImagen_fetch(miArchivo,'test1')});
+
+
+async function uploadImagen_fetch(datfile,datfile2) {
+
+    var totalfiles = datfile.files.length;
+    
+    if(totalfiles > 0 ){
+    
+      var formData = new FormData();
+    
+      // Read selected files
+      for (var index = 0; index < totalfiles; index++) {
+        formData.append("file[]", datfile.files[0]);
+        formData.append("newname", datfile2);
+      }
+    await fetch('loadfile.php',{method: "POST",body: formData})
+    .then(response=>response.json())
+        .then(data=>{
+            console.log(data);
+        });
+
+    }else{
+      alert("Please select a file");
+    }
+    }
