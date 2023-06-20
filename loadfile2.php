f1d322e816ed334c9250d02684341dbe3f8df993
eamarquezh <supericman@outlook.com>
2023-06-19 22:01:56 -0600

    just file load

--- loadfile2.php
+++ loadfile2.php
@@ -1,5 +1,6 @@
 <?php
-$fil = $_POST['newname'];
+
+$fil=$_POST['newname'];
 // Count total files
 $countfiles = count($_FILES['file']['name']);
 
@@ -7,37 +8,38 @@ $countfiles = count($_FILES['file']['name']);
 $upload_location = "upload/";
 
 $count = 0;
-for ($i = 0; $i < $countfiles; $i++) {
+for($i=0;$i < $countfiles;$i++){
 
-    // File name
-    $filename = $_FILES['file']['name'][$i];
+   // File name
+   $filename = $_FILES['file']['name'][$i];
 
-    // File path
-    $parts = explode(".", $filename);
-    $name_n = $_POST['newname'] . "." . end($parts);
-    $path = $upload_location . $_POST['newname'] . "." . end($parts);
+   // File path
+   $parts = explode(".",$filename);
+   $name_n = $_POST['newname'] .".". end($parts);
+   $path = $upload_location . $_POST['newname'] .".". end($parts);
 
-    // file extension
-    $file_extension = pathinfo($path, PATHINFO_EXTENSION);
-    $file_extension = strtolower($file_extension);
+   // file extension
+   $file_extension = pathinfo($path, PATHINFO_EXTENSION);
+   $file_extension = strtolower($file_extension);
 
-    // Valid file extensions
-    $valid_ext = array("png");
+   // Valid file extensions
+   $valid_ext = array("png");
 
-    // Check extension
-    if (in_array($file_extension, $valid_ext)) {
+   // Check extension
+   if(in_array($file_extension,$valid_ext)){
 
-        // Upload file
-        if (copy($_FILES['file']['tmp_name'][$i], $path)) {
-            $count += 1;
-        }
-    }
+      // Upload file
+      if(rename($_FILES['file']['tmp_name'][$i],$path)){
+        $count += 1;
+      } 
+   }
+
 }
 
 $data = array();
 $data['status'] = 'ok';
 $data['result'] = $fil;
 
-echo json_encode($data);
+echo json_encode($data);;
 exit;
 ?>
